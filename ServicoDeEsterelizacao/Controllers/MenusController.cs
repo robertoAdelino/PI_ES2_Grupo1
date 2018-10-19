f80db6779aff53e54790d4e5835bf7810536797a
andretomas1012524 <adtomas16@gmail.com>
2018-10-19 10:06:40 +0100

    Novo sistema de autenticacao

---
+++ ServicoDeEsterelizacao/Controllers/MenusController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServicoDeEsterelizacao.Controllers
+{
+    public class MenusController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+        public IActionResult VHorario()
+        {
+            return View();
+        }
+        public IActionResult GHorario()
+        {
+            return View();
+        }
+        public IActionResult AHorario()
+        {
+            return View();
+        }
+        public IActionResult PSemanal()
+        {
+            return View();
+        }
+        public IActionResult AAlteracao()
+        {
+            return View();
+        }
+        public IActionResult AColaborador()
+        {
+            return View();
+        }
+    }
+}

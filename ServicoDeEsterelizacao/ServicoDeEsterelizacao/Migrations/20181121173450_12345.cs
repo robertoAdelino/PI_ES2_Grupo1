af373a6131e93b890f4fc32bd8bc72190ef98f35
p4ranoyd <roberto_adelino_98@hotmail.com>
2018-11-21 17:37:41 +0000

    Alteracoes

---
+++ ServicoDeEsterelizacao/ServicoDeEsterelizacao/Migrations/20181121173450_12345.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ServicoDeEsterelizacao.Migrations
+{
+    public partial class _12345 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Funcao",
+                columns: table => new
+                {
+                    FuncaoID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Funcao", x => x.FuncaoID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Materialcs",
+                columns: table => new
+                {
+                    MaterialcsId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: false),
+                    Quantidade = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Materialcs", x => x.MaterialcsId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Colaborador",
+                columns: table => new
+                {
+                    ColaboradorId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FuncaoID = table.Column<int>(nullable: false),
+                    Nome = table.Column<string>(maxLength: 50, nullable: false),
+                    Telefone = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: false),
+                    Morada = table.Column<string>(nullable: false),
+                    DataNasc = table.Column<DateTime>(nullable: false),
+                    Cc = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Colaborador", x => x.ColaboradorId);
+                    table.ForeignKey(
+                        name: "FK_Colaborador_Funcao_FuncaoID",
+                        column: x => x.FuncaoID,
+                        principalTable: "Funcao",
+                        principalColumn: "FuncaoID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Colaborador_FuncaoID",
+                table: "Colaborador",
+                column: "FuncaoID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Colaborador");
+
+            migrationBuilder.DropTable(
+                name: "Materialcs");
+
+            migrationBuilder.DropTable(
+                name: "Funcao");
+        }
+    }
+}

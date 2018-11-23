ee45d7b02935853de80c64604dbe7fe70576746f
andretomas1012524 <adtomas16@gmail.com>
2018-11-23 10:01:25 +0000

    Commits 23/11/2018 10:01

---
+++ ServicoDeEsterelizacao/ServicoDeEsterelizacao/Models/Esterelizar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServicoDeEsterelizacao.Models
+{
+    public class Esterelizar
+    {
+        public int EsterelizarID { get; set; }
+
+        public Equipamento Equipamento { get; set; }
+
+        public int EquipamentoID { get; set; }
+
+        public Materialcs Materialcs { get; set; }
+
+        public int MaterialcsID { get; set; }
+    }
+}
---
+++ ServicoDeEsterelizacao/ServicoDeEsterelizacao/Models/NomeEquipamento.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServicoDeEsterelizacao.Models
+{
+    public class NomeEquipamento
+    {
+        public int NomeEquipamentoID { get; set; }
+
+        [Required(ErrorMessage = "Nome não expecificado")]
+        [StringLength(50, MinimumLength = 5)]
+        public string Nome { get; set; }
+
+        public ICollection<Equipamento> Equipamento { get; set; }
+    }
+}

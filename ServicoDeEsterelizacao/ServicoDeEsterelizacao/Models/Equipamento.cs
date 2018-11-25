1e7d27de2282226079d045cf2047776fb4065e12
andretomas1012524 <adtomas16@gmail.com>
2018-11-25 13:28:42 +0000

    add Classes ignoradas

---
+++ ServicoDeEsterelizacao/ServicoDeEsterelizacao/Models/Equipamento.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServicoDeEsterelizacao.Models
+{
+    public class Equipamento
+    {
+        public int EquipamentoID { get; set; }
+
+        [Required] 
+        public int TipoID { get; set; }
+
+        public Tipo Tipo { get; set; }
+
+        public ICollection<Esterilizar> Esterilizar { get; set; }
+    }
+}

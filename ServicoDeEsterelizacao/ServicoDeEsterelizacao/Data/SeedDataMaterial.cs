006d2e366c1b911859ab064529c616f47b1a2b68
andretomas1012524 <adtomas16@gmail.com>
2018-11-25 14:07:12 +0000

    Commits 25/11/2018

--- ServicoDeEsterelizacao/ServicoDeEsterelizacao/Data/SeedDataMaterial.cs
+++ ServicoDeEsterelizacao/ServicoDeEsterelizacao/Data/SeedDataMaterial.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,13 +9,87 @@ namespace ServicoDeEsterelizacao.Models
 {
     public class SeedDataMaterial
     {
+
+        private const string ROLE_ADMINISTRATOR = "Administrator";
+        private const string ROLE_COLAB = "Customer";
+
         public static void Populate(MaterialDbContext db)
         {
-           
+                SeedEquipamento(db);
+                SeedTipo(db);
+                //SeedEsterilizar(db);
                 SeedMaterialcs(db);
                 SeedColaborador(db);
                 SeedFuncao(db);
   
+        }
+
+        private static void SeedTipo(MaterialDbContext db)
+        {
+            if (db.Tipo.Any()) return;
+
+            db.Tipo.AddRange(
+                new Tipo { Nome = "Autoclave"  },//Máquina de 'lavagem' com produtos quimicos
+                new Tipo { Nome = "Descontaminador" }, // Máquina para efetuar descontaminação
+                new Tipo { Nome = "Incenerador"} //'Queima' residuos organicos dos materias
+            );
+        }
+
+        private static void SeedEquipamento(MaterialDbContext db)
+        {
+            if (db.Equipamento.Any()) return;
+
+            db.Equipamento.AddRange(
+                new Equipamento { EquipamentoID= 1,TipoID = 1,Capacidade = 1000},
+                new Equipamento { EquipamentoID = 2 , TipoID = 1, Capacidade = 1500}
+                );
+        }
+
+        private static async void MakeSureRoleExistsAsync(RoleManager<IdentityRole> roleManager, string role)
+        {
+            if (!await roleManager.RoleExistsAsync(role))
+            {
+                await roleManager.CreateAsync(new IdentityRole(role));
+            }
+        }
+
+        public static async Task CreateRolesAndUsersAsync(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            const string ADMIN_USER = "admin@noemail.com";
+            const string ADMIN_PASSWORD = "sECRET$123";
+
+            MakeSureRoleExistsAsync(roleManager, ROLE_ADMINISTRATOR);
+            MakeSureRoleExistsAsync(roleManager, ROLE_COLAB);
+
+            IdentityUser admin = await userManager.FindByNameAsync(ADMIN_USER);
+            if (admin == null)
+            {
+                admin = new IdentityUser { UserName = ADMIN_USER };
+                await userManager.CreateAsync(admin, ADMIN_PASSWORD);
+            }
+
+            if (!await userManager.IsInRoleAsync(admin, ROLE_ADMINISTRATOR))
+            {
+                await userManager.AddToRoleAsync(admin, ROLE_ADMINISTRATOR);
+            }
+        }
+
+        public static async Task CreateTestUsersAsync(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            const string CUSTOMER_USER = "john@noemail.com";
+            const string CUSTOMER_PASSWORD = "sECREDO$123";
+
+            IdentityUser customer = await userManager.FindByNameAsync(CUSTOMER_USER);
+            if (customer == null)
+            {
+                customer = new IdentityUser { UserName = CUSTOMER_USER };
+                await userManager.CreateAsync(customer, CUSTOMER_PASSWORD);
+            }
+
+            if (!await userManager.IsInRoleAsync(customer, ROLE_COLAB))
+            {
+                await userManager.AddToRoleAsync(customer, ROLE_COLAB);
+            }
         }
 
         private static void SeedFuncao(MaterialDbContext db)

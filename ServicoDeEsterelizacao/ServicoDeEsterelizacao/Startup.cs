7a74e3e5ae53a280fd51664b12f0c24357d71abc
andretomas1012524 <adtomas16@gmail.com>
2018-11-28 22:37:20 +0000

    correcao do startup

--- ServicoDeEsterelizacao/ServicoDeEsterelizacao/Startup.cs
+++ ServicoDeEsterelizacao/ServicoDeEsterelizacao/Startup.cs
@@ -84,7 +84,7 @@ namespace ServicoDeEsterelizacao
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app,
             IHostingEnvironment env,
-            MaterialDbContext db,
+            MaterialDbContext db
             /*UserManager<IdentityUser> userManager,
             RoleManager<IdentityRole> roleManager*/)
         {

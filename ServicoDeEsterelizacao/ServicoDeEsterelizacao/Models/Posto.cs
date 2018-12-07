10666854984a4c96652ec6cda1635c91df025025
andretomas1012524 <adtomas16@gmail.com>
2018-12-07 10:51:02 +0000

    Add classes ignoradas

---
+++ ServicoDeEsterelizacao/ServicoDeEsterelizacao/Models/Posto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServicoDeEsterelizacao.Models
+{
+    public class Posto
+    {
+        public int PostoId { get; set; }
+
+        [Required(ErrorMessage ="Por favor, introduza um nome")]
+        public string Nome { get; set; }
+
+        public ICollection<Horario> Horario { get; set; }
+    }
+}
---
+++ ServicoDeEsterelizacao/ServicoDeEsterelizacao/Models/Turno.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServicoDeEsterelizacao.Models
+{
+    public class Turno
+    {
+        public int TurnoId { get; set; }
+
+        [Required(ErrorMessage ="Por favor, insira um nome")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Por favor, introduza a data de inicio")]
+        [DataType(DataType.Date, ErrorMessage = "Data de inicio inválida")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = false)]
+        public DateTime Horainicio { get; set; }
+
+        [Required(ErrorMessage = "Por favor, introduza a data de fim")]
+        [DataType(DataType.Date, ErrorMessage = "Data de fim inválida")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = false)]
+        public DateTime Horafim { get; set; }
+
+        public ICollection<Horario> Horario { get; set; }
+    }
+}

99e0b02e5dd7404585de29875c13b13205cf3679
andretomas1012524 <adtomas16@gmail.com>
2019-01-08 10:42:47 +0000

    add classes ignoradas

---
+++ ServicoDeEsterelizacao/ServicoDeEsterelizacao/Controllers/PostosController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ServicoDeEsterelizacao.Models;
+
+namespace ServicoDeEsterelizacao.Controllers
+{
+    public class PostosController : Controller
+    {
+        private readonly MaterialDbContext _context;
+
+        public PostosController(MaterialDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Postos
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Posto.ToListAsync());
+        }
+
+        // GET: Postos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var posto = await _context.Posto
+                .FirstOrDefaultAsync(m => m.PostoId == id);
+            if (posto == null)
+            {
+                return NotFound();
+            }
+
+            return View(posto);
+        }
+
+        // GET: Postos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Postos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PostoId,Nome")] Posto posto)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(posto);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(posto);
+        }
+
+        // GET: Postos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var posto = await _context.Posto.FindAsync(id);
+            if (posto == null)
+            {
+                return NotFound();
+            }
+            return View(posto);
+        }
+
+        // POST: Postos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("PostoId,Nome")] Posto posto)
+        {
+            if (id != posto.PostoId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(posto);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PostoExists(posto.PostoId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(posto);
+        }
+
+        // GET: Postos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var posto = await _context.Posto
+                .FirstOrDefaultAsync(m => m.PostoId == id);
+            if (posto == null)
+            {
+                return NotFound();
+            }
+
+            return View(posto);
+        }
+
+        // POST: Postos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var posto = await _context.Posto.FindAsync(id);
+            _context.Posto.Remove(posto);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PostoExists(int id)
+        {
+            return _context.Posto.Any(e => e.PostoId == id);
+        }
+    }
+}
---
+++ ServicoDeEsterelizacao/ServicoDeEsterelizacao/Controllers/TurnosController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ServicoDeEsterelizacao.Models;
+
+namespace ServicoDeEsterelizacao.Controllers
+{
+    public class TurnosController : Controller
+    {
+        private readonly MaterialDbContext _context;
+
+        public TurnosController(MaterialDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Turnos
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Turno.ToListAsync());
+        }
+
+        // GET: Turnos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var turno = await _context.Turno
+                .FirstOrDefaultAsync(m => m.TurnoId == id);
+            if (turno == null)
+            {
+                return NotFound();
+            }
+
+            return View(turno);
+        }
+
+        // GET: Turnos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Turnos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TurnoId,Nome,Horainicio,Horafim")] Turno turno)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(turno);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(turno);
+        }
+
+        // GET: Turnos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var turno = await _context.Turno.FindAsync(id);
+            if (turno == null)
+            {
+                return NotFound();
+            }
+            return View(turno);
+        }
+
+        // POST: Turnos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("TurnoId,Nome,Horainicio,Horafim")] Turno turno)
+        {
+            if (id != turno.TurnoId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(turno);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TurnoExists(turno.TurnoId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(turno);
+        }
+
+        // GET: Turnos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var turno = await _context.Turno
+                .FirstOrDefaultAsync(m => m.TurnoId == id);
+            if (turno == null)
+            {
+                return NotFound();
+            }
+
+            return View(turno);
+        }
+
+        // POST: Turnos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var turno = await _context.Turno.FindAsync(id);
+            _context.Turno.Remove(turno);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TurnoExists(int id)
+        {
+            return _context.Turno.Any(e => e.TurnoId == id);
+        }
+    }
+}

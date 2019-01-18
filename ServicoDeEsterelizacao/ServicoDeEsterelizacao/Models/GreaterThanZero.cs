d335a23d3690fb8a1d6b45120060f9395641f2b7
andretomas1012524 <adtomas16@gmail.com>
2019-01-18 19:35:56 +0000

    Class greater Than Zero

---
+++ ServicoDeEsterelizacao/ServicoDeEsterelizacao/Models/GreaterThanZero.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServicoDeEsterelizacao.Models
+{
+    public class GreaterThanZero : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            var x = (int)value;
+            return x > 0;
+        }
+    }
+}
